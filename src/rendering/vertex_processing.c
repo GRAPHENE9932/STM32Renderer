7a1f80c5b17cd69fe7c4d9427e124832f3b84577
Artem Kliminskyi <graphene64@gmail.com>
2024-08-09 12:50:45 +0200

    perf: Optimize the process_triangle procedure, yielding ~19% smaller frametime

--- src/rendering/vertex_processing.c
+++ src/rendering/vertex_processing.c
@@ -10,9 +10,11 @@ void process_triangle(const mat4* mvp, const struct vec3* source, struct vec3* r
     for (uint_fast8_t i = 0; i < 3; i++) {
         struct vec4 v_src = vec4_from_vec3(&source[i], FIXED32_ONE);
         struct vec4 v_out = mat4_mul_vec4(mvp, &v_src);
-        result[i].x = fixed32_div(v_out.x, v_out.w);
-        result[i].y = fixed32_div(v_out.y, v_out.w);
-        result[i].z = fixed32_div(v_out.z, v_out.w);
+        
+        const fixed32 w_inverse = fixed32_div(FIXED32_ONE, v_out.w);
+        result[i].x = fixed32_mul(v_out.x, w_inverse);
+        result[i].y = fixed32_mul(v_out.y, w_inverse);
+        result[i].z = fixed32_mul(v_out.z, w_inverse);
     }
 }
 
